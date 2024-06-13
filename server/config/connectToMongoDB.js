5d228095f03d3cb8c3b869bcb53f9fdd7016b3c4
Adwait S Purao <adwait.purao@spit.ac.in>
2024-06-13 21:14:40 +0530

    initiated server

---
+++ server/config/connectToMongoDB.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const process = require("process");
+
+const connectToMongoDB = async () => {
+  try {
+    // Set the 'strictQuery' option
+    mongoose.set('strictQuery', true);
+
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected!!");
+  } catch (error) {
+    console.log("Failed to connect to MongoDB", error);
+  }
+};
+
+module.exports = connectToMongoDB;
