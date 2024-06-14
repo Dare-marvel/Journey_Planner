0ca05ed64578875ef819497d4cc56a7bca039605
blue56789 <yashsawant0903@gmail.com>
2024-06-14 16:06:55 +0530

    Pages

--- client/src/pages/Map.jsx
+++ client/src/pages/Map.jsx
@@ -1,9 +1,12 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Heading, Image } from "@chakra-ui/react";
 
 export default function Map() {
     return (
-        <Flex h='100dvh' w='100dvw' align='center' justify='center'>
-            Railway Map
+        <Flex h='100dvh' w='100dvw' flexDirection='column' align='center' justify='center'>
+            <Heading my={2}>
+                Railway Map
+            </Heading>
+            <Image src="https://upload.wikimedia.org/wikipedia/commons/5/5e/Mumbai_suburban_rail_map.svg" h='100%' w='100%' />
         </Flex>
     )
 }
--- client/src/pages/StationList.jsx
+++ client/src/pages/StationList.jsx
@@ -1,9 +1,164 @@
-import { Flex } from "@chakra-ui/react";
+import { Card, Flex, Heading, ListItem, UnorderedList } from "@chakra-ui/react";
 
 export default function StationList() {
     return (
-        <Flex h='100dvh' w='100dvw' align='center' justify='center'>
-            Station List
+        <Flex wrap='wrap' justify='center'>
+            <Card p={4} m={4}>
+                <Heading mb={4}>Central Line</Heading>
+                <UnorderedList>
+                    <ListItem>CSMT</ListItem>
+                    <ListItem>Masjid Bunder(MB)</ListItem>
+                    <ListItem>Sandhurst Road(SandhurstR)</ListItem>
+                    <ListItem>Byculla</ListItem>
+                    <ListItem>Chinchpokli</ListItem>
+                    <ListItem>Currey Road(CurreyR)</ListItem>
+                    <ListItem>Parel</ListItem>
+                    <ListItem>Dadar</ListItem>
+                    <ListItem>Matunga</ListItem>
+                    <ListItem>Sion</ListItem>
+                    <ListItem>Kurla</ListItem>
+                    <ListItem>Vidya Vihar(VidyaVihar)</ListItem>
+                    <ListItem>Ghatkopar</ListItem>
+                    <ListItem>Vikhroli</ListItem>
+                    <ListItem>Kanjur Marg(KanjurMarg)</ListItem>
+                    <ListItem>Bhandup</ListItem>
+                    <ListItem>Nahur</ListItem>
+                    <ListItem>Mulund</ListItem>
+                    <ListItem>Thane</ListItem>
+                    <ListItem>Kalwa</ListItem>
+                    <ListItem>Mumbra</ListItem>
+                    <ListItem>Diva</ListItem>
+                    <ListItem>Kopar</ListItem>
+                    <ListItem>Dombivali</ListItem>
+                    <ListItem>Thakurli</ListItem>
+                    <ListItem>Kalyan</ListItem>
+                    <ListItem>Ulhasnagar</ListItem>
+                    <ListItem>Ambernath</ListItem>
+                    <ListItem>Badlapur</ListItem>
+                    <ListItem>Karjat</ListItem>
+                    <ListItem>Khopoli</ListItem>
+                </UnorderedList>
+            </Card>
+            <Card p={4} m={4}>
+                <Heading mb={4}>Harbour Line</Heading>
+                <UnorderedList>
+                    <ListItem>Dockyard</ListItem>
+                    <ListItem>Reay Road(ReayR)</ListItem>
+                    <ListItem>Cotton green(CG)</ListItem>
+                    <ListItem>Sewri</ListItem>
+                    <ListItem>VadalaR</ListItem>
+                    <ListItem>GTB Naga(GTBN)</ListItem>
+                    <ListItem>Chunabhatti</ListItem>
+                    <ListItem>Tilak Nagar(TilakN)</ListItem>
+                    <ListItem>Chembur</ListItem>
+                    <ListItem>Govandi</ListItem>
+                    <ListItem>Mankhurd</ListItem>
+                    <ListItem>Vashi</ListItem>
+                    <ListItem>Sanpada</ListItem>
+                    <ListItem>Juinagar</ListItem>
+                    <ListItem>Nerul</ListItem>
+                    <ListItem>Seawoods Darave(SeawoodsDarave)</ListItem>
+                    <ListItem>CBD Belapur(BelapurCBD)</ListItem>
+                    <ListItem>Kharghar</ListItem>
+                    <ListItem>Manasarovar</ListItem>
+                    <ListItem>Khandeshwar</ListItem>
+                    <ListItem>Panvel</ListItem>
+                </UnorderedList>
+            </Card>
+            <Card p={4} m={4}>
+                <Heading mb={4}>Western Line</Heading>
+                <UnorderedList>
+                    <ListItem>Churchgate</ListItem>
+                    <ListItem>Marine Lines(ML)</ListItem>
+                    <ListItem>Charni Road(CharniR)</ListItem>
+                    <ListItem>Grant Road(GrantR)</ListItem>
+                    <ListItem>Mumbai Central(MC)</ListItem>
+                    <ListItem>Mahalaxmi</ListItem>
+                    <ListItem>Lower Parel(LParel)</ListItem>
+                    <ListItem>Prabhadevi</ListItem>
+                    <ListItem>Matunga Road(MatungaR)</ListItem>
+                    <ListItem>Mahim</ListItem>
+                    <ListItem>Bandra</ListItem>
+                    <ListItem>KharR</ListItem>
+                    <ListItem>Santacruz</ListItem>
+                    <ListItem>Vile Parle(Vile Parle)</ListItem>
+                    <ListItem>Andheri</ListItem>
+                    <ListItem>Jogeshwari</ListItem>
+                    <ListItem>Ram Mandir(RamMandir)</ListItem>
+                    <ListItem>Goregaon</ListItem>
+                    <ListItem>Malad</ListItem>
+                    <ListItem>Kandivali</ListItem>
+                    <ListItem>Borivali</ListItem>
+                    <ListItem>Dahisar</ListItem>
+                    <ListItem>Mira Road(MiraR)</ListItem>
+                    <ListItem>Bhayandar</ListItem>
+                    <ListItem>Naigaon</ListItem>
+                    <ListItem>Vasai Road(VasaiR)</ListItem>
+                    <ListItem>Virar</ListItem>
+                </UnorderedList>
+            </Card>
+            <Card p={4} m={4}>
+                <Heading mb={4}>Thane Vashi Line</Heading>
+                <UnorderedList>
+                    <ListItem>Airoli</ListItem>
+                    <ListItem>Airoli</ListItem>
+                    <ListItem>Rabale</ListItem>
+                    <ListItem>Ghansoli</ListItem>
+                    <ListItem>Kopar Khairane(KoparKhairane)</ListItem>
+                    <ListItem>Turbhe</ListItem>
+                </UnorderedList>
+            </Card>
+            <Card p={4} m={4}>
+                <Heading mb={4}>Indian Railway</Heading>
+                <UnorderedList>
+                    <ListItem>Juchandra</ListItem>
+                    <ListItem>Kaman Road(KamanR)</ListItem>
+                    <ListItem>Kharbao</ListItem>
+                    <ListItem>Bhiwandi</ListItem>
+                    <ListItem>Kopar</ListItem>
+                    <ListItem>Dativali</ListItem>
+                    <ListItem>Nilje</ListItem>
+                    <ListItem>Taloja</ListItem>
+                    <ListItem>Navade Road(NavadeR)</ListItem>
+                    <ListItem>Kalamboli</ListItem>
+                    <ListItem>Panvel</ListItem>
+                </UnorderedList>
+            </Card>
+            <Card p={4} m={4}>
+                <Heading mb={4}>Metro Line</Heading>
+                <UnorderedList>
+                    <ListItem>Ghatkopar</ListItem>
+                    <ListItem>Jagruti Nagar(JagrutiN)</ListItem>
+                    <ListItem>Asalpha</ListItem>
+                    <ListItem>Saki Naka(SakiNaka)</ListItem>
+                    <ListItem>Marol Naka(MarolNaka)</ListItem>
+                    <ListItem>Airport Road(AirportR)</ListItem>
+                    <ListItem>Chakala</ListItem>
+                    <ListItem>Western Express Highway(WesternExpressHighway)</ListItem>
+                    <ListItem>Andheri</ListItem>
+                    <ListItem>Azad Nagar(AzadN)</ListItem>
+                    <ListItem>D N Nagar(DNNagar)</ListItem>
+                    <ListItem>Versova</ListItem>
+                </UnorderedList>
+            </Card>
+            <Card p={4} m={4}>
+                <Heading mb={4}>Mono Rail Line</Heading>
+                <UnorderedList>
+                    <ListItem>Chembur</ListItem>
+                    <ListItem>VNP Marg Junction</ListItem>
+                    <ListItem>Fertilizer Township(FertilizerTownship)</ListItem>
+                    <ListItem>Bharat Petroleum(BP)</ListItem>
+                    <ListItem>Mysore Colony(MysoreColony)</ListItem>
+                    <ListItem>Bhakti Park(BhaktiPark)</ListItem>
+                    <ListItem>VadalaR</ListItem>
+                    <ListItem>GTB Nagar(GTBN)</ListItem>
+                    <ListItem>Antop Hill(AntopHill)</ListItem>
+                    <ListItem>Acharya Atre Nagar(AcharyaAtreNagar)</ListItem>
+                    <ListItem>Vadala Bridge(VadalaBridge)</ListItem>
+                    <ListItem>Dadar</ListItem>
+                    <ListItem>Naigaon</ListItem>
+                </UnorderedList>
+            </Card>
         </Flex>
     )
 }
